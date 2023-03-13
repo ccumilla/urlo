c30672a1a96f0c47de44cdc812503445d133b4f5
ccumilla <127745080+ccumilla@users.noreply.github.com>
2023-03-13 15:45:42 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int     numero = 27;
+  double  numerolungo = 666666666;
+  float   numeroconlavirgola = 420.69;
+  char    carattere = 'c';
+
+  printf("il cazzo è: %c, il numero è: %d, il numeroconlavirgola è: %f\n", carattere, numero, numeroconlavirgola);
+
+}
