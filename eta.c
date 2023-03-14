33eb2cc9f65947801236ecf355878242e418e8fd
ccumilla <127745080+ccumilla@users.noreply.github.com>
2023-03-14 10:22:01 +0100

    Add files via upload

---
+++ eta.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{ 
+    int x;
+    char genere;
+printf("quanti anni hai?" "in che genere ti identifichi?");
+scanf ("%d %c", &x, &genere);
+printf("ho %d anni, " "sono %c \n", x, genere);
+}
