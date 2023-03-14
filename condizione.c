0ab0cc4ffec42750a40c24713535bcc671f897cc
ccumilla <127745080+ccumilla@users.noreply.github.com>
2023-03-14 11:20:15 +0100

    Add files via upload

---
+++ condizione.c
@@ -0,0 +1,19 @@
+#include  <stdio.h>
+
+int main()
+{
+    int x;
+    printf("aspe amò quanti anni c'hai?");
+    scanf("%d", &x );
+
+     
+    if (x >= 18)
+    {
+        printf("se trombaaa\n");
+    }
+
+    else
+    {
+        printf("ar gabbio ez\n");
+    }
+}
