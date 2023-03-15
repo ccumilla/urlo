1bb022cce5e1bdfbd8d57b46d4fec45474074835
ccumilla <127745080+ccumilla@users.noreply.github.com>
2023-03-15 16:53:40 +0100

    Add files via upload

---
+++ ultimo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int n;
+printf("inserire un numero ");
+scanf("%d", &n);
+printf("ïnserisci %d numeri ", n);
+int array[n];
+int i;
+while(i<n)
+{
+scanf( "%d", &array[n]);
+i++;
+
+}
+
+
+}
