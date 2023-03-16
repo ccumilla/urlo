529c7081e8d8fba19d0c7d4817fc95d0971df4c4
ccumilla <127745080+ccumilla@users.noreply.github.com>
2023-03-16 15:29:42 +0100

    Add files via upload

---
+++ noncapisconulla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+char stringa[100];
+
+
+while(stringa[0]!= 'S' || stringa[1]!='T' || stringa[2]!='O' || stringa[3]!='P')
+    {
+    printf("inserire una stringa: ");
+scanf(" %s", stringa);
+printf(" %s\n", stringa);
+
+}
+}
