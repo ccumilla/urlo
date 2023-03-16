963cfbe356242515b7d6d1a6060cca0b25840b9d
ccumilla <127745080+ccumilla@users.noreply.github.com>
2023-03-16 16:57:04 +0100

    Add files via upload

---
+++ potenza.c
@@ -0,0 +1,18 @@
+#include <stdio.h> <math.h>
+
+void pow(base, esponente)
+{
+  printf("%d", pow);
+}
+int main()
+{
+    int base;
+    int esponente;
+
+printf("inserire base: ");
+scanf("%d", &base );
+printf("inserire esponente: ");
+scanf("%d", &esponente );
+
+
+}
