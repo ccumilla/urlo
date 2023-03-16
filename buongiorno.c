afecb57cb8479539a16ce4c7431b40de4a35cf16
ccumilla <127745080+ccumilla@users.noreply.github.com>
2023-03-16 12:04:08 +0100

    Add files via upload

---
+++ buongiorno.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+
+
+   
+   char stringa[]="";
+   printf("scrivi una parola\n");
+   scanf(" %s", stringa);
+   int i=0;
+   while(stringa[i] != '\0')
+   {
+     
+   i++;
+   }
+   printf("%d\n", i);
+
+
+}
